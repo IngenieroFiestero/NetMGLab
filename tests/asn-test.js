8871a03d717162654ed549217c1354fdcd290222
Samuel Garcés Marín <Samuel Garcs Marn>
2017-04-04 22:18:45 +0200

    Angular2 added

--- tests/asn-test.js
+++ tests/asn-test.js
@@ -7,36 +7,64 @@ const SNMPAgentModel = require('../models/snmp_agent');
 const os = require('os');
 var windows = os.platform() == 'win32';
 
-if(windows){
-    console.log("Multiprocessing udp not implemented yet");
-}
+
+
+/**
+ * snmpget -v 2c -c public 127.0.0.1 .1.3.6.1.2.1.1.4.0
+ */
 if (cluster.isMaster) {
     var db = mongoose.connect(config.db.uri);
     mongoose.connection.once('connected', function () {
-        var newAgent = new SNMPAgentModel();
-        newAgent.port = 161;
-        newAgent.name = "Pepe Botella";
-        newAgent.readOnlyCommunity = "public";
-        newAgent.readWriteCommunity = "private";
-        newAgent.device = mongoose.Types.ObjectId();
-        newAgent.save((err, doc) => {
+        SNMPAgentModel.findOne({ port: 161 }, (err, doc) => {
             if (err) {
-                console.log(err);
+                var newAgent = new SNMPAgentModel();
+                newAgent.port = 161;
+                newAgent.name = "Pepe Botella";
+                newAgent.readOnlyCommunity = "public";
+                newAgent.readWriteCommunity = "private";
+                newAgent.device = mongoose.Types.ObjectId();
+                newAgent.save((err, doc) => {
+                    var id;
+                    if (err) { } else {
+                        console.log("New agent created");
+                        console.log(newAgent);
+                        //We are the master
+                        for (var i = 0; i < 2; i++) {
+                            var worker = i == 0 ? cluster.fork({ lector: true, mongoid: newAgent._id }) : cluster.fork();
+                            process.lector = worker.process.pid;
+                        }
+                        cluster.on('exit', function (worker, code, signal) {
+                            //If a child dies spawn another
+                            console.log("Error1: " + code);
+                            console.log(worker);
+                            if (worker.process.pid == process.lector) {
+                                var worker = cluster.fork({ lector: true, mongoid: newAgent._id });
+                                process.lector = worker.process.pid;
+                            }
+                        });
+                        snmpMultiServer.init();
+                    }
+                });
             } else {
-                console.log("New agent created");
-                console.log(newAgent);
+                for (var i = 0; i < 2; i++) {
+                    var worker = i == 0 ? cluster.fork({ lector: true, mongoid: doc._id }) : cluster.fork();
+                    process.lector = worker.process.pid;
+                }
+                cluster.on('exit', function (worker, code, signal) {
+                    //If a child dies spawn another
+                    console.log("Error2: " + code);
+                    console.log(worker.process.pid + " - " + process.lector);
+                    if (worker.process.pid == process.lector) {
+                        var worker = cluster.fork({ lector: true, mongoid: doc._id });
+                        process.lector = worker.process.pid;
+                    }
+                });
+                snmpMultiServer.init();
             }
         });
+
     });
-    //We are the master
-    for (var i = 0; i < 2; i++) {
-        var worker = i == 0 ? cluster.fork({lector : true}) : cluster.fork();
-        cluster.on('exit', function (worker, code, signal) {
-            //If a child dies spawn another
-            console.log("Error: " + code);
-        });
-    }
-    snmpMultiServer.init();
+
 } else {
     snmpMultiServer.init();
     var db = mongoose.connect(config.db.uri);
@@ -44,6 +72,7 @@ if (cluster.isMaster) {
         //Each child connects to mongo and reuses the connection
         console.log('New worker: ' + process.pid);
         if (process.env.lector) {
+            console.log("Somos el lector");
             const rl = readline.createInterface({
                 input: process.stdin,
                 output: process.stdout
@@ -57,7 +86,7 @@ if (cluster.isMaster) {
             rl.on('line', (input) => {
                 if (input == "launch") {
                     console.log("Sending Launch");
-                    snmpMultiServer.launchSNMPAgent(mongoose.Types.ObjectId("58e2b48e765b35261001554d"));
+                    snmpMultiServer.launchSNMPAgent(process.env.mongoid);
                 }
             });
         }
