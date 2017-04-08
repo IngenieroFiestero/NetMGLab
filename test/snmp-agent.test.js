4d8f81da6fd7ee9f0c62b510101f112131784a7f
Samuel <ingenierofiestero@gmail.com>
2017-04-08 20:05:48 +0200

    antiBruteForce snmp module

---
+++ test/snmp-agent.test.js
@@ -0,0 +1,49 @@
+const assert = require('assert');
+const snmp = require('../lib/snmp');
+const SNMPAgent = require('../lib/snmp-agent');
+const dgram = require('dgram')
+var agente = new SNMPAgent({ port: 8002, device: "paco", readOnlyCommunity: "pub", readWriteCommunity: "priv", name: "paco agent" });
+
+
+
+describe('SNMP AGENT', function () {
+    describe('#start()', function () {
+        it('should start', function (done) {
+            agente.on('listening', () => {
+                done();
+            });
+            agente.start();
+        });
+    });
+    describe('#BadMessageRecieved()', function () {
+        it('should send a fake response', function (done) {
+            //Simulate sending a bad snmp request with a bad community, then the agent responses a fake message to mitigate bruteforce attack
+            var fakePckt = new snmp.SNMPPacket(snmp.SNMP_VERSIONS.SNMPv2c,'comm',new snmp.PDU(
+                snmp.PDU_TYPES.GetRequestPDU,
+                10,
+                0,
+                0,
+                [new snmp.VarBind(snmp.TAG.Integer,17,'1.2.3.16')]
+            ));
+            var sock = dgram.createSocket('udp4');
+            sock.on('message',(msg)=>{
+                done();
+            });
+            sock.bind(9001,'localhost',()=>{
+                var buff = snmp.encode(fakePckt,snmp.ENCODING_RULES.BER);
+                sock.send(buff,0,buff.length,8002,'localhost');
+            });
+            
+        });
+    });
+    
+    describe('#stop()', function () {
+        it('should stop', function (done) {
+            agente.on('stop', () => {
+                done();
+            });
+            agente.stop();
+        });
+    });
+});
+
