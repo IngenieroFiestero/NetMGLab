bf3f8ecd9a137873517f707ca35eda9443ad010e
Samuel <ingenierofiestero@gmail.com>
2017-04-08 16:41:28 +0200

    add mocha for travis

--- test/mib.test.js
+++ test/mib.test.js
@@ -1,11 +1,11 @@
 const assert = require('assert');
 var fs = require("fs");
-var smi = require("../lib/smi");
+var mib = require("../lib/mib");
 //-------------------------------- SYNTAX TEST ---------------------------------------
 var syntaxResults = require('./syntax-test.json');
 /*
 for (var i = 0; i < syntaxResults.textSequence.length; i++) {
-        assert.deepEqual(smi.parseSyntax(syntaxResults.textSequence[i]).returned, syntaxResults.results[i].returned, 'Error in SYNTAX test: ' + syntaxResults.textSequence[i]);
+        assert.deepEqual(mib.parseSyntax(syntaxResults.textSequence[i]).returned, syntaxResults.results[i].returned, 'Error in SYNTAX test: ' + syntaxResults.textSequence[i]);
 }*/
 
 //-------------------------------- OBJECT TYPE TEST ---------------------------------------
@@ -13,20 +13,20 @@ for (var i = 0; i < syntaxResults.textSequence.length; i++) {
 var objectTypeResults = require('./object-type-test.json');
 /*
 for (var i = 0; i < objectTypeResults.textSequence.length; i++) {
-        assert.deepEqual(smi.parseObjectType(objectTypeResults.textSequence[i]).returned, objectTypeResults.results[i].returned, 'Error in OBJECT TYPE test: ' + objectTypeResults.textSequence[i]);
+        assert.deepEqual(mib.parseObjectType(objectTypeResults.textSequence[i]).returned, objectTypeResults.results[i].returned, 'Error in OBJECT TYPE test: ' + objectTypeResults.textSequence[i]);
 }*/
 describe('MIB TEST', function () {
         describe('#SYNTAX', function () {
                 it('Checking syntax', function () {
                         for (var i = 0; i < syntaxResults.textSequence.length; i++) {
-                                assert.deepEqual(smi.parseSyntax(syntaxResults.textSequence[i]).returned, syntaxResults.results[i].returned,'Error in SYNTAX test: ' + syntaxResults.textSequence[i]);
+                                assert.deepEqual(mib.parseSyntax(syntaxResults.textSequence[i]).returned, syntaxResults.results[i].returned,'Error in SYNTAX test: ' + syntaxResults.textSequence[i]);
                         }
                 });
         });
         describe('#OBJECT TYPE', function () {
                 it('Checking Object Type', function () {
                         for (var i = 0; i < objectTypeResults.textSequence.length; i++) {
-                                assert.deepEqual(smi.parseObjectType(objectTypeResults.textSequence[i]).returned, objectTypeResults.results[i].returned,'Error in OBJECT TYPE test: ' + objectTypeResults.textSequence[i]);
+                                assert.deepEqual(mib.parseObjectType(objectTypeResults.textSequence[i]).returned, objectTypeResults.results[i].returned,'Error in OBJECT TYPE test: ' + objectTypeResults.textSequence[i]);
 
                         }
                 });
